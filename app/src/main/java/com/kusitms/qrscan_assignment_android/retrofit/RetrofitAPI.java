3a74bf56f8724b350de6ca2a9125db22e166bf40
Jeongminyooa <jmjmjmz732002@naver.com>
2022-09-27 21:49:38 +0900

    REFACTOR : 주문별 시리얼 넘버 검증

--- app/src/main/java/com/kusitms/qrscan_assignment_android/retrofit/RetrofitAPI.java
+++ app/src/main/java/com/kusitms/qrscan_assignment_android/retrofit/RetrofitAPI.java
@@ -1,10 +1,13 @@
 package com.kusitms.qrscan_assignment_android.retrofit;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface RetrofitAPI {
-    @GET("/validate")
-    Call<ResponseResult> validateSerialNumber(@Query("serialNumber") String serialNumber);
+    @POST("/order/validate")
+    Call<ResponseResult> validateSerialNumber(@Body String serialNumber);
 }
